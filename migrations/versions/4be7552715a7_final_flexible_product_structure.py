45270a92e7ef4ec86b91c5161090a75dfe19e6e6
Jarno <jarno.blomme@telenet.be>
2025-09-09 02:11:53 +0200

    Add final flexible product structure migration with complete table definitions

--- migrations/versions/4be7552715a7_final_flexible_product_structure.py
+++ migrations/versions/4be7552715a7_final_flexible_product_structure.py
@@ -1,8 +1,8 @@
 """Final flexible product structure
 
-Revision ID: a5b79cd0555e
+Revision ID: 4be7552715a7
 Revises: 
-Create Date: 2025-09-09 01:41:04.526361
+Create Date: 2025-09-09 02:11:26.787928
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a5b79cd0555e'
+revision = '4be7552715a7'
 down_revision = None
 branch_labels = None
 depends_on = None
